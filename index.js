6ac69458c1fe2ff7697adba5294fcee71596204c
Darknab <117382045+Darknab@users.noreply.github.com>
2024-03-09 21:12:51 +0100

    Remove unused consts

--- index.js
+++ index.js
@@ -1,11 +1,3 @@
-const librairie = document.querySelector('#librairie');
-const outil = document.querySelector('#outil');
-const subOutil = document.querySelector('.sub-outil');
-const gentamycine = document.querySelector('#gentamycine');
-const subGentamycine = document.querySelector('.sub-gentamycine');
-const modele = document.querySelector('#modele');
-const subModele = document.querySelector('.sub-modele');
-
 const elements = document.querySelectorAll('.element');
 
 elements.forEach((element) => {
@@ -16,8 +8,3 @@ elements.forEach((element) => {
     }
   })
 })
-
-
-
-
-
